0f93e57f7509b6bea748eb0a583f5bd01db47848
Peter EJ Davies <psq95@users.noreply.github.com>
2016-04-28 14:22:24 -0400

    Added files via upload

--- gdmlFile/root/worldDraw.c
+++ gdmlFile/root/worldDraw.c
@@ -2,13 +2,13 @@
 void worldDraw() {
     gSystem->Load("libGeom");
     gSystem->Load("libGdml");
-    TGeoManager *geom = TGeoManager::Import("test.gdml");
+    TGeoManager *geom = TGeoManager::Import("survey_ideal_all.gdml");
 
     TList *matList = geom->GetListOfMaterials();
     TIter matNext( matList );
     while( mat = (TGeoMaterial*) matNext() )
     {
-        mat->SetTransparency( 80 );
+        mat->SetTransparency( 0 );
     }
     
     /*TGeoMaterial *mat = geom->GetMaterial("mat_vacuum");
@@ -21,7 +21,41 @@ void worldDraw() {
     
     TObjArray *volList = geom->GetListOfVolumes();
     TIter volNext( volList );
-    while( vol = (TGeoVolume*) volNext() ) { vol->SetLineColor( kRed ); }
+    while( vol = (TGeoVolume*) volNext() )
+    {
+        TString *volName = new TString( vol->GetName() );
+        if( volName->Contains("ref") )
+        {
+            cout << "ref";
+            vol->SetLineColor( kWhite );
+        }
+        else if( volName->Contains("target") )
+        {
+            cout << "target";
+            vol->SetLineColor( kBlack );
+        }
+        else if( volName->Contains("mod") )
+        {
+            cout << "mod";
+            vol->SetLineColor( kRed );
+        }
+        else if( volName->Contains("fidn") )
+        {
+            cout << "fidn";
+            vol->SetLineColor( kOrange );
+        }
+        else if( volName->Contains("fidi") )
+        {
+            cout << "fidi";
+            vol->SetLineColor( kSpring );
+        }
+        else
+        {
+            cout << "default";
+            vol->SetLineColor( kGray );
+        }
+        cout << " " << volName->Data() << "\n";
+    }
     
     /*TGeoVolume *vol0 =  geom->GetVolume("vol_module0");
       vol0->SetLineColor( kGreen );
